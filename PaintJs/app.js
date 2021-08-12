d6a4cbc8fa340048bebe7b99f251f97d00725a87
wjdwl002 <wjdwl001@khu.ac.kr>
2021-08-12 12:13:11 +0900

    색, 굵기 변경 기능

--- PaintJs/app.js
+++ PaintJs/app.js
@@ -1,27 +1,55 @@
 const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = 700;
+canvas.height = 700;
+
+
 let painting = false; //클릭 상태
 
 function onMouseMove(event){
     //console.log(event.offsetX, event.offsetY); // 여기서 offset 값만 필요
     const x = event.offsetX;
     const y = event.offsetY;
+    if(!painting){
+        changeWidth(event);
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+    }else {
+        ctx.lineTo(x, y);
+        ctx.stroke();
+    }
 }
+
+function changeColor(event){
+    const color = event.target.style.backgroundColor;
+    ctx.strokeStyle = color;
+}
+
+function changeWidth(event){
+    ctx.lineWidth = document.getElementById("jsRange").value;
+}
+
 function stopPainting(){
     painting = false;
 }
 
-function onMouseDown(event){
+function startPainting(){
     painting = true;
 }
 
-function onMouseUp(event){
-    stopPainting();
+function onMouseDown(event){
+    startPainting();
 }
+
 
 if(canvas){
     canvas.addEventListener('mousemove', onMouseMove);
-    canvas.addEventListener('mousedown', onMouseDown);
-    canvas.addEventListener('mouseup', onMouseUp);
-    canvas.addEventListener('mouseleave', onMouseLeave);
+    canvas.addEventListener('mousedown', startPainting);
+    canvas.addEventListener('mouseup', stopPainting);
+    canvas.addEventListener('mouseleave',stopPainting)
 
 }
+
+var colors = document.getElementsByClassName("jsColor")
+Array.from(colors).forEach(color => color.addEventListener("click", changeColor));
