3d4e3c241a469a47b4fe0e48d9117c912d5b4440
wjdwl002 <wjdwl001@khu.ac.kr>
2021-08-16 23:13:19 +0900

    themes

--- PaintJs/app.js
+++ PaintJs/app.js
@@ -1,18 +1,25 @@
 const canvas = document.getElementById("jsCanvas");
+const body = document.getElementsByTagName('body');
+const slider = document.getElementById("jsRange");
+const sidebar = document.getElementsByTagName('label');
 const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColor")
 const mode = document.getElementById('jsMode');
+const clear = document.getElementById('jsClear');
 const themes = document.getElementsByClassName("jsTheme");
+
 
 //색상표
 var colorsSample = [
-    ['#6F3B48', '#522632', '#522632', '#ECD7D2', '#F3E4DF', '#FFEEE7'], 
-    ['#C6DFD6', '#ACCCC4', '#464545', '#C4BCB8', '#E2DFD8', '#EDECEA'],
-    ['#EAE9E7', '#CDD5D3', '#98A6A6', '#6B7275', '#745F67', '#9BA980'],
-    ['#F5F3F2', '#FDDFDE', '#F66465', '#AB3B3D', '#56555C', '#DFDEDC'],
-    ['#CB4242', '#8F1E33', '#461E2E', '#87374F', '#87374F',]
+    ['#CB4242', "#8F1E33", "#461E2E", "#87374F", "#C9718B", "#FE93B0"], 
+    ["#FBEFCB", "#F9CDAC", "#F4A688", "#E1876A", "#987162", "#E9B4A1"],
+    ["#F9D5D3", "#ECA4A6", "#807F89", "#99A89E" ,"#BBC7BA", "#D7DBD1"],
+    ["#C2C4A0", "#868E74", "#AD5988", "#E5B293", "#E9C9B6", "#ECE2E3"],
+    ["#B1CAE5", "#635D58", "#C3B767", "#DFCD85", "#EBE49F", "#F7F6D3"],
+    ["#E2D8E7", "#E3C4D9", "#B85480", "#F1E1B4", "#EEE9D6", "#F0EFE5"],
+    ["#DAD1D0", "#B5AAAA", "#A8C4B8", "#C4DBD4", "#D9E8E6", "#E5EFEE"],
+    ["#201E1D", "#101C1E", "#304F54", "#638B9A", "#A5C8DF", "#D8E7DB"]
 ]
-ctx.strokeStyle = colorsSample[0][5];
 
 canvas.width = 700;
 canvas.height = 700;
@@ -25,12 +32,22 @@ function changeTheme(event){
     for (var i =0; i<colors.length; i++){
         colors[i].style.backgroundColor=colorsSample[themeIndex][i];
     }   
+    ctx.clearRect(0,0,canvas.width, canvas.height);
+    canvas.style.backgroundColor = "#ffffff";
+    body[0].style.background = colorsSample[themeIndex][5];
+    sidebar[0].style.background = colorsSample[themeIndex][4];
+    sidebar[0].style.color = colorsSample[themeIndex][2];
+    ctx.strokeStyle = colorsSample[themeIndex][0];
+}
+
+function clearCanvas(){
+    ctx.clearRect(0,0,canvas.width, canvas.height);
 }
 
 function handleModeClick(){
     if(filling === true){
         filling = false;
-        mode.innerHTML = "Fill";
+        mode.innerHTML = "Draw";
     }
     else {
         filling = true;
@@ -55,6 +72,7 @@ function onMouseMove(event){
 
 function changeColor(event){
     const color = event.target.style.backgroundColor;
+    console.log(color);
     if(filling) canvas.style.backgroundColor = color;
     else ctx.strokeStyle = color;
 }
@@ -87,12 +105,12 @@ if(mode) {
     mode.addEventListener('click', handleModeClick);
 }
 
+if(clear){
+    clear.addEventListener('click', clearCanvas);
+}
 
 
 Array.from(colors).forEach(color => color.addEventListener("click", changeColor));
 Array.from(themes).forEach(theme => theme.addEventListener("click", changeTheme));
 
-for (var i =0; i<colors.length; i++){
-    colors[i].style.backgroundColor=colorsSample[0][i];
-}
 
